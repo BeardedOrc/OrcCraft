33e9a219a1829cda792bf5e2a5ab9e0162946f0a
BuildTools <unconfigured@null.spigotmc.org>
2018-09-08 22:53:58 -0600

    update

---
+++ src/main/java/me/beardedorc/orccraft/commands/InfoCommand.java
@@ -0,0 +1,29 @@
+package me.beardedorc.orccraft.commands;
+
+import me.beardedorc.orccraft.OrcCraft;
+import me.beardedorc.orccraft.commands.utilities.SubCommand;
+import org.bukkit.entity.Player;
+
+public class InfoCommand extends SubCommand {
+    private OrcCraft plugin = OrcCraft.getInstance();
+
+    @Override
+    public void onCommand(Player player, String[] args) {
+        player.sendMessage("This info is cool!");
+    }
+
+    @Override
+    public String name() {
+        return plugin.commandManager.info;
+    }
+
+    @Override
+    public String info() {
+        return "";
+    }
+
+    @Override
+    public String[] aliases() {
+        return new String[0];
+    }
+}
---
+++ src/main/java/me/beardedorc/orccraft/utilities/ConfigManager.java
@@ -0,0 +1,57 @@
+package me.beardedorc.orccraft.utilities;
+
+import me.beardedorc.orccraft.OrcCraft;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigManager {
+
+    private OrcCraft plugin = OrcCraft.getInstance();
+
+    public ConfigManager() {};
+
+    // ---------  Files and Configs  --------------
+    public FileConfiguration customItemsCFG;
+    public File customItems;
+    // --------------------------------------------
+
+    public void setup() {
+        if (!(plugin.getDataFolder().exists())) {
+            plugin.getDataFolder().mkdir();
+        }
+
+        customItems = new File(plugin.getDataFolder(),"customItems.yml");
+
+        if (!customItems.exists()) {
+            try {
+                customItems.createNewFile();
+            }catch (IOException e) {
+                Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "Could not create customItems.yml file.");
+            }
+        }
+
+        customItemsCFG = YamlConfiguration.loadConfiguration(customItems);
+    }
+
+    public  FileConfiguration getCustomItemsCFG() {
+        return customItemsCFG;
+    }
+    
+    public void saveCustomItems() {
+
+        try {
+            customItemsCFG.save(customItems);
+        } catch (IOException e) {
+            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "The custom items file could not be saved");
+        }
+    }
+
+    public void reloadCustomItems() {
+        customItemsCFG = YamlConfiguration.loadConfiguration(customItems);
+    }
+}
